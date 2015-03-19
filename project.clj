2f5e2a34bd776b0432b3ef9dfc6f815b2449d9d2
Dylan Butman <dbutman@gmail.com>
2015-03-19 13:12:38 -0400

    update deps

--- project.clj
+++ project.clj
@@ -2,18 +2,18 @@
   :global-vars  {*warn-on-reflection* true}
 
   :dependencies [
-    [org.clojure/clojure "1.7.0-alpha4"]
-    [org.clojure/clojurescript "0.0-2511"]
-    [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
-    [datascript "0.7.2"]
-    [rum "0.1.0"]
+    [org.clojure/clojure "1.7.0-alpha5"]
+    [org.clojure/clojurescript "0.0-3126"]
+    [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+    [datascript "0.8.1"]
+    [rum "0.2.6"]
   ]
 
   :plugins [
     [lein-cljsbuild "1.0.3"]
   ]
 
-  :cljsbuild { 
+  :cljsbuild {
     :builds [
       { :id "prod"
         :source-paths  ["src"]
